ebd51a3ac0fb700a66cd4fc073e8eedc1254fd2f
myself-6264 <pingcreator626@gmail.com>
2024-11-03 11:26:52 +0530

    Update and rename twrp_infinix-x6812.mk to twrp_Infinix-X6812.mk

--- twrp_Infinix-X6812.mk
+++ twrp_Infinix-X6812.mk
@@ -14,16 +14,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from x6812 device
-$(call inherit-product, device/infinix/infinix-x6812/device.mk)
+$(call inherit-product, device/infinix/Infinix-X6812/device.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRIVATE_BUILD_DESC="vnd_x6812_h6912-user 11 RP1A.200720.011 435922 release-keys"
 
-BUILD_FINGERPRINT := Infinix/X6812-GL/Infinix-x6812:11/RP1A.200720.011/230921V645:user/release-keys
+BUILD_FINGERPRINT := Infinix/X6812-GL/Infinix-X6812:11/RP1A.200720.011/230921V645:user/release-keys
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := infinix-x6812
-PRODUCT_NAME := twrp_infinix-x6812
+PRODUCT_DEVICE := Infinix-X6812
+PRODUCT_NAME := twrp_Infinix-X6812
 PRODUCT_BRAND := Infinix
-PRODUCT_MODEL := Infinix-x6812
+PRODUCT_MODEL := Infinix-X6812
 PRODUCT_MANUFACTURER := infinix
